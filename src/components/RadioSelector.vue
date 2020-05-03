f3b42a246a5518dee6942466e6dbac8131f21e9e
chingchinglcc <30460357+chingchinglcc@users.noreply.github.com>
2020-05-03 15:10:54 -0600

    reverted radio button changes

--- src/components/RadioSelector.vue
+++ src/components/RadioSelector.vue
@@ -1,28 +1,26 @@
 <template>
-  <div :class="'radio-selector ' + className">
-    <label v-for="({name, imgSrc}, colorCode) in items" :key="colorCode" :for="name">
-      <!-- {{items}} -->
-      <img :src="`require(…/assets/products/${imgSrc})`" alt />
-      <!-- <img v-if="showImage" :src="`~assets/flowers/${imgSrc}`" /> -->
-      <img v-if="showImage" src="../static/assets/flowers/NH-red_cosmos-icon.png" />
-      <div class="radio-name">
-        <span v-if="name === 'red2'">Red (Bred)</span>
-        <span v-else>{{name}}</span>
-      </div>
+  <div class="radio-selector">
+    <label v-for="{ name, colorCode } in items" :key="colorCode" :for="name">
+      <input
+        type="radio"
+        :name="radioName"
+        :value="name"
+        v-model="setected"
+        @change="emitChecked"
+      />
+      <span v-if="name === 'red2'">Red (Bred)</span>
+      <span v-else>{{ name }}</span>
     </label>
   </div>
 </template>
 
 <script>
-// import abs from ''
-
 export default {
   name: "RadioSelector",
-  props: ["items", "radioName", "showImage", "className"],
+  props: ["items", "radioName"],
   data: function() {
     return {
-      setected: "",
-      path: ""
+      setected: ""
     };
   },
   methods: {
@@ -33,30 +31,4 @@ export default {
 };
 </script>
 
-<style scoped>
-[type="radio"] {
-  position: absolute;
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
-
-/* IMAGE STYLES */
-[type="radio"] + img {
-  cursor: pointer;
-}
-
-/* CHECKED STYLES */
-[type="radio"]:checked + img {
-  outline: 2px solid #f00;
-}
-
-.radio-selector-flex {
-  display: flex;
-  justify-content: space-around;
-}
-
-.radio-selector-flex label {
-  text-align: center;
-}
-</style>
+<style scoped></style>
